8ca22193d27eed72f81f05c4e7f2fc2ace681385
eren23 <erenak1996@gmail.com>
2020-07-06 20:27:55 +0300

    Commit before the blog posts

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Connect to our db finally
+connectDB();
+//Init middleware
+app.use(express.json({ extended: false }));
+//Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(` Server is running on port ${PORT}`));
